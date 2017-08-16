08664852519c98670c0eeab87077cbfe224132e6
yhawong <yhawong@hotmail.com>
2017-08-16 15:06:24 -0700

    File upload

--- Server/Common/FileUploadException.js
+++ Server/Common/FileUploadException.js
@@ -4,8 +4,8 @@ var errorcodes = require('./errorcode.json');
 var ExceptionBase = require('./exceptionbase.js');
 
 module.exports = class FileUploadException extends ExceptionBase{  
-    constructor(multipartErr){
-        var httpcode = multipartErr.statusCode || 400;
-        super(multipartErr.message, 'FileUploadException', errorcodes.GenericStorageException, httpcode, errorcodes.GenericStorageException);
+    constructor(error, successfulFileUpload){
+        var httpcode = error.statusCode || 400;
+        super(error.message, 'FileUploadException', errorcodes.GenericStorageException, httpcode, errorcodes.GenericStorageException);
     }
 }
